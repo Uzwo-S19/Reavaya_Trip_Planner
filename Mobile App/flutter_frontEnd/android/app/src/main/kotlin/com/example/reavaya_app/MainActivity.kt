59c72e696ff94955bdc7e4317176d198164a2534
Uzwothe Emmanuel Mannzhi <125763850+Uzwo-S19@users.noreply.github.com>
2026-08-06 13:08:55 +0200

    Front end files

---
+++ Mobile App/flutter_frontEnd/android/app/src/main/kotlin/com/example/reavaya_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reavaya_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Mobile App/flutter_frontEnd/test/widget_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:reavaya_app/main.dart';
+
+import 'package:reavaya_app/Payment/point_purchase_page.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const App()); // Use App() instead of const MyApp()
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+}
